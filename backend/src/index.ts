d8bac574c9aaa667a596e1e9e339da8fd339453e
Jan Gross <jan.gross.cze@gmail.com>
2026-03-23 20:00:41 +0100

    mocked backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,9 +5,36 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
+const ENDPOINT = "https://jsonplaceholder.typicode.com/usersY"
+
 app.get("/", (_req, res) => {
-  res.json({ status: "ok" });
+  res.send("Hello from the backend!");
 });
+
+app.get("/api/testApi", async (_req, res) => {
+  try {
+    const response = await fetch(ENDPOINT)
+
+
+    if (!response.ok) {
+      throw new Error(`
+        route: api/testAPI,
+        ENDPOINT: ${ENDPOINT}
+        - Response is not ok!
+      `);
+    }
+
+    const data = await response.json()
+
+    return res.json({ data })
+  } catch (error) {
+    console.error("Error fetching data:", error);
+
+    return res
+      .status(500)
+      .json({ error: "Failed to fetch data" });
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Backend running on http://localhost:${PORT}`);
